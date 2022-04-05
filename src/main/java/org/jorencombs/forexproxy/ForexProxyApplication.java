00969fea42583ca191a66e6c11cbbd38b2331f28
JorenCombs <JorenCombs@users.noreply.github.com>
2022-04-05 23:06:59 +0900

    First commit (Spring Initializr boilerplate hello world code)

---
+++ src/main/java/org/jorencombs/forexproxy/ForexProxyApplication.java
@@ -0,0 +1,13 @@
+package org.jorencombs.forexproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexProxyApplication.class, args);
+	}
+
+}
